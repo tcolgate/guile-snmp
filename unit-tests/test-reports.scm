02cfdcff8107951e5e87cd79c6e70632292ba82c
Tristan Colgate <w107255@pinot.(none)>
2010-01-05 11:34:35 +0000

    Rename peer to host, peer just sounds too flashy

--- unit-tests/test-reports.scm
+++ unit-tests/test-reports.scm
@@ -9,7 +9,7 @@
   
 (define-method (test-get (self <test-reports>))
   (assert-equal "Testing Guile SNMP"
-                (session #:peer "127.0.0.1:10161" 
+                (session #:host "127.0.0.1:10161" 
                   ((get (snmp-parse-oid "sysLocation.0"))))))
 
 (exit-with-summary (run-all-defined-test-cases))
