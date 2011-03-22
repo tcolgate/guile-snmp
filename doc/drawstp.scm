02456375bc4bfdc5621e983f372b3d29d3ab8f0c
Tristan Colgate <tcolgate@gmail.com>
2011-03-22 17:03:52 +0000

    Add options handling, makes it more useful and serves as an example of using getopt-long from a script

--- doc/drawstp.scm
+++ doc/drawstp.scm
@@ -1,8 +1,9 @@
-#!/home/tcolgate/local/bin/snmp-shell -s
+#!/usr/local/bin/snmp-shell -s
 !#
 
 ; draw a graph of the current spanning tree configuration for 
-; a given vlan
+; a given vlan This example also uses getopt-long ot process
+; command line arguments.
 
 (use-mibs BRIDGE-MIB)
 
@@ -25,39 +26,53 @@
     (lambda()
       (format #t "~a:~a" p m))))
 
-(default-session #:community "K9gYhRWkXhX3Afx")
+(define (drawvlan port)
+  (session #:community "K9gYhRWkXhX3Afx")
 
-(format #t "digraph g {~%")
-(format #t "rankdir=BT~%")
-;
-(map 
-  (lambda(item)
-    (let ((name (car item))
-          (node (cdr item)))
-      (session #:host (slot-ref node 'ip) 
-               #:community (slot-ref node 'community)
-               #:version (slot-ref node 'version)
-        (let* ((brname   ((get sysName.0)))
-               (brmac    (mac ((get dot1dBaseBridgeAddress.0))))
-               (brprio   (prio ((get dot1dStpPriority.0))))
-               (brid     (bridgeid brmac brprio))
-               (brdr     (mac ((get dot1dStpDesignatedRoot.0)))))
-        
-          (format #t "#Name:            ~a~%" brname)
-          (format #t "#Bridge Mac:      ~a~%" brmac)
-          (format #t "#Bridge Prio:     ~a(~4,'0x)~%" brprio ((get dot1dStpPriority.0)))
-          (format #t "#Bridge Id:       ~a~%" brid)
-          (format #t "#Designated Root: ~a~%" brdr)
-          (format #t "br~a[label = \"~a\"];~%" brid name)
-          (if (not (equal? brid brdr))
-            (let* ((brdrprtid   ((get dot1dStpRootPort.0)))
-                   (drprtifindx ((get (+ dot1dBasePortIfIndex brdrprtid))))
-                   (drprtname   ((get (+ ifName drprtifindx))))
-                   (drprtbr     (mac ((get (+ dot1dStpPortDesignatedBridge brdrprtid))))))
-               ;(format #t "DR Port:         ~a~%" drprtname)
-               ;(format #t "Next Bridge:     ~a~%" drprtbr)))))))
-               (format #t "\"br~a\" -> \"br~a\" [ label = \"~a\" ] ;~%" brid drprtbr drprtname )))))))
-  nodes)
+    (format port "digraph g {~%")
+    (format port "rankdir=BT~%")
 
-(format #t "}~%")
+    (map 
+      (lambda(item)
+        (let ((name (car item))
+              (node (cdr item)))
+          (session #:host (slot-ref node 'ip) 
+                   #:community (slot-ref node 'community)
+                   #:version (slot-ref node 'version)
+            (let* ((brname   ((get sysName.0)))
+                   (brmac    (mac ((get dot1dBaseBridgeAddress.0))))
+                   (brprio   (prio ((get dot1dStpPriority.0))))
+                   (brid     (bridgeid brmac brprio))
+                   (brdr     (mac ((get dot1dStpDesignatedRoot.0)))))
+          
+              (format port "br~a[label = \"~a\"];~%" brid name)
+              (if (not (equal? brid brdr))
+                (let* ((brdrprtid   ((get dot1dStpRootPort.0)))
+                       (drprtifindx ((get (+ dot1dBasePortIfIndex brdrprtid))))
+                       (drprtname   ((get (+ ifName drprtifindx))))
+                       (drprtbr     (mac ((get (+ dot1dStpPortDesignatedBridge brdrprtid))))))
+                   (format port "\"br~a\" -> \"br~a\" [ label = \"~a\" ] ;~%" brid drprtbr drprtname )))))))
+    nodes)
 
+    (format port "}~%"))
+
+(define (main args)
+  (let* ((option-spec '((version (single-char #\v) (value #f))
+                        (help    (single-char #\h) (value #f))))
+         (options (getopt-long args option-spec))
+         (help-wanted (option-ref options 'help #f))
+         (version-wanted (option-ref options 'version #f)))
+    (if (or version-wanted help-wanted)
+        (begin
+          (if version-wanted
+              (format #t "drawStp version 0.1~%"))
+          (if help-wanted
+              (format #t "\
+getopt-long-example [options]
+  -v, --version    Display version
+  -h, --help       Display this help
+")))
+        (begin
+          (drawvlan #t)))))
+
+(main (script-arguments))
