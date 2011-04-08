2e85b70d4b5f783f183084949b71bf847d2b9acb
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2011-04-08 10:47:26 +0100

    Update to allow vlan passed on the CLI

--- doc/drawstp.scm
+++ doc/drawstp.scm
@@ -1,9 +1,11 @@
-#!/usr/local/bin/snmp-shell -s
+#!/home/tcolgate/local/bin/snmp-shell -s
 !#
 
 ; draw a graph of the current spanning tree configuration for 
 ; a given vlan This example also uses getopt-long ot process
 ; command line arguments.
+
+(use-modules (ice-9 regex))
 
 (use-mibs BRIDGE-MIB)
 
@@ -26,7 +28,15 @@
     (lambda()
       (format #t "~a:~a" p m))))
 
-(define (drawvlan port)
+(define (decolonify str)
+     (regexp-substitute/global #f ":" str 'pre 'post))
+
+(define (deslashify str)
+     (regexp-substitute/global #f "/" str 'pre "_" 'post))
+
+(default-session #:timeout 10000000) ;some 3750s take an age to response to BRIDGE-MIB
+
+(define (drawvlan vlan port)
   (format port "digraph g {~%")
   (format port "rankdir=BT~%")
   (session #:community "K9gYhRWkXhX3Afx"
@@ -34,70 +44,59 @@
       (lambda(item)
         (let ((name (car item))
               (node (cdr item)))
-;         (format (current-error-port) "Node: ~A~%" name)
+         (format (current-error-port) "Node: ~A~%" name)
           (session #:host (slot-ref node 'ip)
-                   #:community (slot-ref node 'community)
+                   #:community (if (equal? vlan "1")
+                                 (slot-ref node 'community)
+                                 (string-append (slot-ref node 'community) "@" vlan))
                    #:version (slot-ref node 'version)
-            (let* ((brname   ((get sysName.0)))
-                   (brmacval ((get dot1dBaseBridgeAddress.0))))
-              (if (not (equal? 'noSuchObject brmacval))
-              (let*(
-                   (brmac    (mac brmacval))
-                   (brprio   (prio ((get dot1dStpPriority.0))))
-                   (brid     (bridgeid brmac brprio))
-                   (brdr     (mac ((get dot1dStpDesignatedRoot.0)))))
+            (let ((brname    ((get sysName.0)))
+                  (brmacval  ((get dot1dBaseBridgeAddress.0)))
+                  (brprioval ((get dot1dStpPriority.0))))
+              (if (and (not (equal? 'noSuchObject brmacval))
+                       (not (equal? 'noSuchInstance brprioval)))
+                (let*((brprio   (prio brprioval))
+                      (brmac    (mac brmacval))
+                      (brid     (bridgeid brmac brprio))
+                      (brdr     (mac ((get dot1dStpDesignatedRoot.0)))))
 
-              (if (equal? brid brdr)
-                (format #t "br~a[label = \"~a\", penwidth = 3];~%" (decolonify brid) name)
-                (let* ((brdrprtid   ((get dot1dStpRootPort.0)))
-                       (drprtifindx ((get (+ dot1dBasePortIfIndex brdrprtid))))
-                       (drprtname   ((get (+ ifName drprtifindx))))
-                       (drprtbr     (mac ((get (+ dot1dStpPortDesignatedBridge brdrprtid))))))
-                   (format #t "br~a[label = \"~a\"];~%" (decolonify brid) name)
-                   (begin
-                     ; Draw DR link
-                     (format #t "\"br~a\" -> \"br~a\" [ label = \"~a\" ] ;~%"
-                             (decolonify brid)
-                             (decolonify drprtbr)
-                             (deslashify drprtname) )
-                     (catch 'walkend
-                       (lambda()
-                         (let ((dprtstatefunc (walk-func dot1dStpPortState)))
-                           (let dprtloop ((dprtstate (dprtstatefunc)))
-                             (if (equal? 2 (dprtstate)) ; blocked port 
-                               (let* ((blkprt (iid dprtstate))
-                                      (blkbrid (mac ((get (+ dot1dStpPortDesignatedBridge blkprt)))))
-                                      (blkprtifindex ((get (+ dot1dBasePortIfIndex blkprt))))
-                                      (blkprtname ((get (+ ifName blkprt)))))
-                                 (format #t "\"br~a\" -> \"br~a\" [ label = \"~a\", style = dashed ] ;~%"
-                                         (decolonify brid)
-                                         (decolonify blkbrid)
-                                         (deslashify blkprtname))))
+                  (if (equal? brid brdr)
+                    (format #t "br~a[label = \"~a\", penwidth = 3];~%" (decolonify brid) name)
+                    (let* ((brdrprtid   ((get dot1dStpRootPort.0)))
+                           (drprtifindx ((get (+ dot1dBasePortIfIndex brdrprtid))))
+                           (drprtname   ((get (+ ifName drprtifindx))))
+                           (drprtbr     (mac ((get (+ dot1dStpPortDesignatedBridge brdrprtid))))))
+                       (format #t "br~a[label = \"~a\"];~%" (decolonify brid) name)
+                       (begin
+                         ; Draw DR link
+                         (format #t "\"br~a\" -> \"br~a\" [ label = \"~a\" ] ;~%"
+                                 (decolonify brid)
+                                 (decolonify drprtbr)
+                                 (deslashify drprtname) )
+                         (catch #t
+                           (lambda()
+                             (let ((dprtstatefunc (walk-func dot1dStpPortState)))
+                               (let dprtloop ((dprtstate (dprtstatefunc)))
+                                 (if (equal? 2 (dprtstate)) ; blocked port 
+                                   (let* ((blkprt (iid dprtstate))
+                                          (blkbrid (mac ((get (+ dot1dStpPortDesignatedBridge blkprt)))))
+                                          (blkprtifindex ((get (+ dot1dBasePortIfIndex blkprt))))
+                                          (blkprtname ((get (+ ifName blkprt)))))
+                                     (format #t "\"br~a\" -> \"br~a\" [ label = \"~a\", style = dashed ] ;~%"
+                                             (decolonify brid)
+                                             (decolonify blkbrid)
+                                             (deslashify blkprtname))))
                              (dprtloop (dprtstatefunc)))))
-                       (lambda(key . args)
-                         #t)))))))))))
+                           (lambda(key . args)
+                             #t)))))))))))
     nodes))
 
   (format port "}~%"))
 
 (define (main args)
-  (display (command-line))(newline)
-  (let* ((option-spec '((version (single-char #\v) (value #t))
-                        (help    (single-char #\h) (value #t))))
-         (options (getopt-long args option-spec))
-         (help-wanted (option-ref options 'help #f))
-         (version-wanted (option-ref options 'version #f)))
-    (if (or version-wanted help-wanted)
-        (begin
-          (if version-wanted
-              (format #t "drawStp version 0.1~%"))
-          (if help-wanted
-              (format #t "\
-getopt-long-example [options]
-  -v, --version    Display version
-  -h, --help       Display this help
-")))
-        (drawvlan #t))))
+  (if (eq? args '())
+   (drawvlan "1" #t)
+   (drawvlan (car args) #t)))
 
 (main (script-arguments))
 
