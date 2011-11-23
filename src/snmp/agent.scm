7c0e5be6f626d266a010753c987958e99ce9e2a1
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2011-11-23 09:07:10 +0000

    Initial agent support

---
+++ src/snmp/agent.scm
@@ -0,0 +1,13 @@
+;;-------------------------------------------------------------------
+;; Copyright (C) 2009,2010 Tristan Colgate 
+;;
+;; agent.scm - This file defines routines for simple  agent support
+;;
+;;-------------------------------------------------------------------
+
+(define-module (snmp angent)
+  #:use-module (oop goops)
+  #:use-module (snmp net-snmp)
+  #:export (
+  ))
+
